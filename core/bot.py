660df74798a618ac43f8cb348de0da0ee2da0200
Anay <madinuranay13@gmail.com>
2026-05-27 20:18:35 +0530

    fix price type issue pandas scalar

--- core/bot.py
+++ core/bot.py
@@ -13,7 +13,7 @@ class Bot:
             return "NO DATA", 0
 
         signal = generate_signal(df)
-        price = df["Close"].iloc[-1]
+        price = float(df["Close"].iloc[-1].item())
 
         if signal == "BUY":
             self.portfolio.buy(symbol, price)
